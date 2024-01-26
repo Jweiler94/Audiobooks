33d5438dc1eec53d93848de94c8dc77d57d1442d
Jweiler94 <jweiler94@hotmail.com>
2024-01-26 10:45:51 -0600

    initial commit

---
+++ Backend/src/main/java/JW/projects/Audiobooks/AudiobooksApplication.java
@@ -0,0 +1,13 @@
+package JW.projects.Audiobooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiobooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiobooksApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/JW/projects/Audiobooks/AudiobooksApplicationTests.java
@@ -0,0 +1,13 @@
+package JW.projects.Audiobooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiobooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
