97cc92f6790646869a150602ffd703af47f5ead8
Jweiler94 <jweiler94@hotmail.com>
2024-01-27 10:43:30 -0600

    started setting up file structure- components, pages, routes, AuthUser, error, main

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error";
+import { AuthUserProvider } from "./AuthUser";
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
