012083ba9fd1de3cc10ca8cd6c634f22c41e56dd
Jweiler94 <jweiler94@hotmail.com>
2024-01-29 18:57:06 -0600

    working on audiobook menu component sorting data pulled from api

--- frontend/client/src/components/AudiobookComponents/audiobookmenu.jsx
+++ frontend/client/src/components/AudiobookComponents/audiobookmenu.jsx
@@ -1,12 +1,40 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const AudioMenu = () => {
-      const [genres, setGenres] = useState("");
-      const [authors, setAuthors] = useState("");
-      const [translators, setTranslators] = useState("");
-      const [sections, setSections] = useState("");
+      const [link, setLink] = useState("");
+      const [downloadLink, setDownloadLink] = useState("");
+      const [author, setAuthor] = useState("");
+      const [title, setTitle] = useState("");
+      const [year, setYear] = useState(null);
+      const [description, setDescription] = useState("");
+      const [chapters, setChapters] = useState("");
+      const [textLink, setTextLink] = useState("");
+      const [length, setLength] = useState("");
 
-      
+      const [booksData, setBooksData] = useState(null);
+      const [authorList, setAuthorList] = useState(null);
+
+      useEffect(() => {
+            const fetchData = async () => {
+                  try {
+                        const booksResponse = await axios.get(
+                              "https://librivox.org/api/feed/audiobooks/?^all&format=json"
+                        );
+
+                        setBooksData(booksResponse.data);
+                  } catch(error) {
+                        console.error("Error fetching data", error);
+                  }
+            };
+
+            fetchData();
+
+      }, []);
+
+     
+
+
 }
 
 export default AudioMenu;
